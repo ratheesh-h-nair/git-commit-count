91a9d3fc10fac46e749891ee406b90bc331f9900
Ratheesh H Nair <43346970+ratheesh-h-nair@users.noreply.github.com>
2022-07-26 12:41:38 +0530

    Add files via upload
    initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+BASE_URL = "https://api.github.com"
+LIST_USERS = "/users"
+SEARCH = "/search"
+REPO_QUERY = "/repositories?q=fork=true"
+REPOS ="/repos"
+COMMITS = "/commits"
+
+LIST_OF_REPOS = "list of Repos that having most trending with highest commits"
+
+headers = {
+    'authorization':"Bearer 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"
+}
