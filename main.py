a31a6bba94f700b9c1e19591d01840f51c1d1d7e
Ratheesh H Nair <43346970+ratheesh-h-nair@users.noreply.github.com>
2022-07-26 13:01:38 +0530

    Update main.py
    Updated the return statement

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def index():
         n = request.args.get('repo-count',default = 1,type=int)
         m = request.args.get('commit-count',default = 1,type=int)
         repo_list = get_repo_list(n,m)
-        return jsonify(make_response(True,repo_list,LIST_OF_REPOS))
+        return jsonify(repo_list)
     except Exception as e:
         import sys
         exc_type, exc_obj, exc_tb = sys.exc_info()
